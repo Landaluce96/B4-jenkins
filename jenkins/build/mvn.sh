6e55088217222b467c9584eece5cdb368abc1bda
Landaluce <landaluce1996@gmail.com>
2023-05-03 12:43:15 +0200

    change workspace path on mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "********************"
 echo "*** BUILDING JAR ***"
 echo "********************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/b4-jenkins
+WORKSPACE=/home/landaluce/jenkins/jenkins-data/jenkins_home/workspace/b4-jenkins
 
 docker run --rm -v $WORKSPACE/maven-log4j:/app -v /root/.m2:/root/.m2 -w /app maven "$@"
